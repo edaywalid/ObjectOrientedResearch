3469efa065699fb7f74d1daf8c666e7830758d7b
Adel2411 <hadjarabadel.2411@gmail.com>
2024-04-26 15:05:26 +0100

    implemented number of imported packages V1

---
+++ Metrics/Adel/IMPLEMENTATIONS/src/Main.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String filePath = args[0] + ".java";
+        Set<String> importedPackages = getImportedPackages(filePath);
+        System.out.println("\t------------- Imported Packages -------------");
+        for (String p : importedPackages) {
+            System.out.println("[" + p + "]");
+        }
+    }
+
+    public static Set<String> getImportedPackages(String filePath) {
+        Set<String> packages = new HashSet<>();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                line = line.trim();
+                if (line.startsWith("import ")) {
+                    String packageName = line.substring(7, line.length() - 1); // 7 is the length of "import "
+                    if (packageName.endsWith(".*")) {
+                        String rootPackage = packageName.substring(0, packageName.length() - 2);
+                        addSubPackages(packages, rootPackage);
+                        packages.add(rootPackage);
+                    } else {
+                        packages.add(packageName);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return packages;
+    }
+
+
+    // This method is used to count the number of subpackages in a given package
+  public static void addSubPackages(Set<String> subPackages, String packageName) {
+      Package[] packages = Package.getPackages();
+      for (Package p : packages) {
+          if (p.getName().startsWith(packageName + ".")) {
+              subPackages.add(p.getName());
+              addSubPackages(subPackages, p.getName());
+          }
+      }
+  }
+}
---
+++ Metrics/Adel/IMPLEMENTATIONS/src/Test.java
@@ -0,0 +1,6 @@
+import java.awt.*;
+import java.time.*;
+import java.util.*;
+import java.io.BufferedReader;
+
+public class Test {}
