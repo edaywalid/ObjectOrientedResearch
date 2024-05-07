9158d7d122b53fcc4b2cfd2069051890fe5e1d9b
Adel2411 <hadjarabadel.2411@gmail.com>
2024-05-08 00:22:54 +0100

    Correcting NumberOfImportedClassesUsed cases

--- Metrics/Adel/IMPLEMENTATIONS/src/Main.java
+++ Metrics/Adel/IMPLEMENTATIONS/src/Main.java
@@ -3,21 +3,21 @@ import java.util.Set;
 public class Main {
 
   public static void main(String[] args) {
-    NumberOfImportedClasses NOIC = new NumberOfImportedClasses(args[0]);
-    Set<String> importedPackages = NOIC.getImportedPackages();
+    NumberOfImportedClasses noic = new NumberOfImportedClasses(args[0]);
+    Set<String> importedPackages = noic.getImportedPackages();
 
     System.out.println("\t------------- Imported Packages -------------");
-    for (String p : importedPackages) {
-      System.out.println("[" + p + "]");
-    }
+    //    for (String p : importedPackages) {
+    //      System.out.println("[" + p + "]");
+    //    }
     System.out.println("number of imported packages: " + importedPackages.size());
 
     System.out.println("\n\n------------- Used Classes -------------");
-    NumberOfImportedClassesUsed NOICU = new NumberOfImportedClassesUsed(args[0]);
-    Set<String> UsedClasses = NOICU.getNumberOfUsedClasses();
-    for (String p : UsedClasses) {
+    NumberOfImportedClassesUsed noicu = new NumberOfImportedClassesUsed(args[0]);
+    Set<String> usedClasses = noicu.getNumberOfUsedClasses();
+    for (String p : usedClasses) {
       System.out.println("[" + p + "]");
     }
-    System.out.println("\nnumber of used classes: " + UsedClasses.size());
+    System.out.println("\nnumber of used classes: " + usedClasses.size());
   }
 }
