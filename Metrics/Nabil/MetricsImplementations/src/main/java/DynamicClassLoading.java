3f45e027112b0675bab33ab0f04ea7a212ef1c6f
Nabil KARA <karanabil420@gmail.com>
2024-05-07 19:07:09 +0100

    changed structure

---
+++ Metrics/Nabil/MetricsImplementations/src/main/java/DynamicClassLoading.java
@@ -0,0 +1,25 @@
+import java.io.File;
+import java.net.URL;
+import java.net.URLClassLoader;
+
+public class DynamicClassLoading{
+
+    public static void main(String[] args) throws Exception {
+        String filePath = "file.java";
+        String UnixStylefilePath = filePath.replace("\\", "/");
+        compileJavaFile(filePath);
+        File classFile = new File(filePath);
+        URLClassLoader classLoader = URLClassLoader.newInstance(new URL[]{classFile.toURI().toURL()});
+        String fileName;
+        fileName = UnixStylefilePath.split("/")[filePath.split("/").length - 1];
+        Class<?> clazz = classLoader.loadClass(fileName.split("\\.")[0]);
+        System.out.println("Number of Interfaces Implemented" + InterfaceCounter.countInterfaces(clazz));
+    }
+    public static void compileJavaFile(String filePath) throws Exception {
+        Process compileProcess = Runtime.getRuntime().exec("javac " + filePath);
+        int exitCode = compileProcess.waitFor();
+        if (exitCode != 0) {
+            throw new RuntimeException("Compilation failed.");
+        }
+    }
+}
