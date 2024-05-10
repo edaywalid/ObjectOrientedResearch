62ba9345a698cb0b8810202a614628cf78c34589
Adel2411 <hadjarabadel.2411@gmail.com>
2024-05-10 03:57:35 +0100

    added Number of imported Classes Implementation into the Console App

--- METRIC_CALCULTOR_APP/CONSOLE_APP/src/Metrics/ExampleMetric.java
+++ METRIC_CALCULTOR_APP/CONSOLE_APP/src/Metrics/ExampleMetric.java
@@ -5,22 +5,22 @@ import Model.Result;
 
 public class ExampleMetric extends ClassLevelMetric {
 
-    public ExampleMetric(String metricName) {
-        super(metricName);
-    }
+  public ExampleMetric(String metricName) {
+    super(metricName);
+  }
 
-    int result(){
-        // This is a dummy method to test the functionality of the test cases
-        return 124;
-    }
+  int result() {
+    // This is a dummy method to test the functionality of the test cases
+    return 124;
+  }
 
-    @Override
-    public float calculate(String file_path) {
-        return result();
-    }
+  @Override
+  public float calculate(String file_path) {
+    return result();
+  }
 
-    @Override
-    public Result execute(String file_path) {
-        return new Result(this.metricName, String.valueOf(this.calculate(file_path)));
-    }
+  @Override
+  public Result execute(String file_path) {
+    return new Result(this.metricName, String.valueOf(this.calculate(file_path)));
+  }
 }
--- METRIC_CALCULTOR_APP/CONSOLE_APP/src/Repository/Model_Repository.java
+++ METRIC_CALCULTOR_APP/CONSOLE_APP/src/Repository/Model_Repository.java
@@ -1,35 +1,30 @@
 package Repository;
 
 import Metrics.ExampleMetric;
-import Metrics.wmc;
-import Model.ClassLevelMetric;
+import Metrics.NumberOfImportedClasses;
 import Model.Metric;
-import Model.PackageLevelMetric;
-
 import java.util.ArrayList;
 import java.util.List;
 
-public class Model_Repository{
-    ArrayList<Metric> classLevelMetrics ;
-    ArrayList<Metric> packageLevelMetrics;
+public class Model_Repository {
+  ArrayList<Metric> classLevelMetrics;
+  ArrayList<Metric> packageLevelMetrics;
 
+  public Model_Repository() {
+    classLevelMetrics =
+        new ArrayList<>(
+            List.of(
+                new ExampleMetric("Example Metric"),
+                new NumberOfImportedClasses("Number of Imported Classes")));
 
-    public Model_Repository(){
-        classLevelMetrics = new ArrayList<>(List.of(
-            new wmc("Weighted Method Count"),
-                new ExampleMetric("Example Metric")
-        ));
+    packageLevelMetrics = new ArrayList<>(List.of());
+  }
 
-        packageLevelMetrics = new ArrayList<>(List.of(
+  public ArrayList<Metric> getClassLevelMetrics() {
+    return classLevelMetrics;
+  }
 
-        ));
-    }
-
-    public ArrayList<Metric> getClassLevelMetrics() {
-        return classLevelMetrics;
-    }
-
-    public ArrayList<Metric> getPackageLevelMetrics() {
-        return packageLevelMetrics;
-    }
+  public ArrayList<Metric> getPackageLevelMetrics() {
+    return packageLevelMetrics;
+  }
 }
