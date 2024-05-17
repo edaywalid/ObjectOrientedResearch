45970e2485e25e91fb0003ca0e96073976bad282
Nabil KARA <karanabil420@gmail.com>
2024-05-17 09:30:22 +0100

    new way to count used methods used from imports using anltr4

--- METRIC_CALCULTOR_APP/CONSOLE_APP/src/Repository/Model_Repository.java
+++ METRIC_CALCULTOR_APP/CONSOLE_APP/src/Repository/Model_Repository.java
@@ -19,8 +19,7 @@ public class Model_Repository {
                     new handledExceptions("Number Of Handled Exceptions"),
                     new InterfaceCounter("Number of Implemented Interfaces"),
                     new numberOfUsedMethodsFromImports("Number of Used Methods From Imports"),
-                    new testUsedMethodsFromImports("Test used methods"),
-                    new usedMethodsFromImportsANTLR("ANTLR")
+                    new UsedMethodsFromImportsANTLR("ANTLR")
                     ));
 
     packageLevelMetrics = new ArrayList<>(List.of());
