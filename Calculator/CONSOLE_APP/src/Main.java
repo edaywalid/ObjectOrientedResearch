4ee080fa8ea816872f63237e287e7f69e7255748
unknown <mimobn2004@gmail.com>
2024-05-19 11:50:27 +0100

    working calc

---
+++ Calculator/CONSOLE_APP/src/Main.java
@@ -0,0 +1,8 @@
+
+import View.ConsoleView;
+
+public class Main {
+    public static void main(String[] args) {
+        ConsoleView cv=new ConsoleView();
+    }
+}
---
+++ Calculator/CONSOLE_APP/src/View/ConsoleView.java
@@ -0,0 +1,5 @@
+package View;
+
+public class ConsoleView {
+        Calculator cal = new Calculator();
+}
