04ec0b907a7f5d92a23274ff4ff9585599900fcc
edaywalid <becharwalid9@gmail.com>
2024-05-24 02:47:58 +0100

    updated the structure

--- Metrics/Nabil/MetricsImplementations/src/test/java/CustomException.java
+++
@@ -1,8 +0,0 @@
-package MetricsImplementations.src.test.java;
-
-public class CustomException extends Exception{
-    @Override
-    public String getMessage() {
-        return "Custom Exception";
-    }
-}
